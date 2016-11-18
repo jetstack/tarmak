0e7e4c800bf1dc5d54b27894f8ff921a176b6532
Sean Turner <sean.turner@jetstack.io>
2016-11-18 11:44:37 +0000

    missed one

--- manifests/init.pp
+++ manifests/init.pp
@@ -90,7 +90,7 @@ define etcd::install (
 }
 
 define etcd::config (
-  String $cluster_name,
+  String $etcd_cluster_name,
   String $etcd_version,
   String $etcd_initial_cluster,
   Integer $client_port,
@@ -105,7 +105,7 @@ define etcd::config (
 }
 
 define etcd::systemd (
-  String $cluster_name,
+  String $etcd_cluster_name,
   String $etcd_version,
 )
 {
