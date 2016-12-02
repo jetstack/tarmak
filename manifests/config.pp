268716c85e46450c5a3c026ed26939eff3c27ada
Sean Turner <sean.turner@jetstack.io>
2016-12-02 12:34:36 +0000

    Escapes

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,8 +5,8 @@
 class vault_client::config {
 
   exec { "In dev mode get CA":
-    command => "/bin/bash /etc/sysconfig/vault && /usr/bin/vault vault read -field=certificate $cluster_name/pki/etcd-k8s/cert/ca > /etc/pki/ca-trust/source/anchors/etcd-k8s.pem",
-    unless => "/bin/bash /etc/sysconfig/vault && grep ${/usr/bin/vault vault read -field=certificate $cluster_name/pki/etcd-k8s/cert/ca} /etc/pki/ca-trust/source/anchors/etcd-k8s.pem",
+    command => "/bin/bash /etc/sysconfig/vault && /usr/bin/vault vault read -field=certificate \$cluster_name/pki/etcd-k8s/cert/ca > /etc/pki/ca-trust/source/anchors/etcd-k8s.pem",
+    unless => "/bin/bash /etc/sysconfig/vault && grep \$\{/usr/bin/vault vault read -field=certificate \$cluster_name/pki/etcd-k8s/cert/ca\} /etc/pki/ca-trust/source/anchors/etcd-k8s.pem",
     notify => Exec["update CA trust"],
   }
 
