4df86e494439814bdd0aa379f33006f31c07ad1c
Sean Turner <sean.turner@jetstack.io>
2016-12-02 13:31:42 +0000

    Unless is still correct

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class vault_client::config {
 
   exec { "In dev mode get CA":
     command => "/bin/bash -c 'source /etc/sysconfig/vault; /usr/bin/vault read -address=\$VAULT_ADDR -field=certificate \$CLUSTER_NAME/pki/etcd-k8s/cert/ca > /etc/pki/ca-trust/source/anchors/etcd-k8s.pem'",
-    onlyif  => "/bin/bash -c 'source /etc/sysconfig/vault; /usr/bin/vault read -address=\$VAULT_ADDR -field=certificate \$CLUSTER_NAME/pki/etcd-k8s/cert/ca | diff - /etc/pki/ca-trust/source/anchors/etcd-k8s.pem'",
+    unless  => "/bin/bash -c 'source /etc/sysconfig/vault; /usr/bin/vault read -address=\$VAULT_ADDR -field=certificate \$CLUSTER_NAME/pki/etcd-k8s/cert/ca | diff - /etc/pki/ca-trust/source/anchors/etcd-k8s.pem'",
     notify => Exec["update CA trust"],
   }
 
