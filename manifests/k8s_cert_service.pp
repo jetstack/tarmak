7714fa0aacc495253d72840bb0f65ed65658be0c
Sean Turner <sean.turner@jetstack.io>
2016-12-06 15:36:38 +0000

    First cut of k8s vault scripts

---
+++ manifests/k8s_cert_service.pp
@@ -0,0 +1,20 @@
+define vault_client::k8s_cert_service (
+  String $k8s_component,
+  String $frequency,
+)
+{
+
+  include ::systemd
+
+  file { "/usr/lib/systemd/system/k8s-${k8s_component}-cert.service":
+    ensure => file,
+    content => template('vault_client/k8s-cert.service.erb'),
+  } ~>
+  Exec['systemctl-daemon-reload']
+
+  file { "/usr/lib/systemd/system/etcd-${k8s_component}-cert.timer":
+    ensure => file,
+    content => template('vault_client/k8s-cert.service.erb'),
+  } ~>
+  Exec['systemctl-daemon-reload']
+}
