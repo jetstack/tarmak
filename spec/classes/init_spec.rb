c47357b29945dba4304dca0054efba42e9e7d93b
Christian Simon <simon@swine.de>
2016-12-07 09:39:48 +0000

    Mock vault client instead of importing module

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
+
+
 describe 'etcd' do
+  let(:pre_condition) {[
+    'class vault_client{}'
+  ]}
   context 'with default values for all parameters' do
     it { should contain_class('etcd') }
   end
