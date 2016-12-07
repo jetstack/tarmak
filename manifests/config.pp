a3421036d3762d9bd0b19f0eee71587832556916
Sean Turner <sean.turner@jetstack.io>
2016-12-07 13:49:41 +0000

    Better

--- manifests/config.pp
+++ manifests/config.pp
@@ -25,14 +25,33 @@ class vault_client::config {
     home   => '/var/lib/etcd',
   }
 
-if $vault_client::role == 'master' {
+  if $vault_client::role == 'master' {
     exec { 'In dev mode get CA for k8s':
       command => "/bin/bash -c 'source /etc/sysconfig/vault; /usr/bin/vault read -address=\$VAULT_ADDR -field=certificate \$CLUSTER_NAME/pki/etcd-k8s/cert/ca > /etc/pki/ca-trust/source/anchors/etcd-k8s.pem'",
       unless  => "/bin/bash -c 'source /etc/sysconfig/vault; /usr/bin/vault read -address=\$VAULT_ADDR -field=certificate \$CLUSTER_NAME/pki/etcd-k8s/cert/ca | diff -P /etc/pki/ca-trust/source/anchors/etcd-k8s.pem -'",
       notify  => Exec['update CA trust'],
     }
+
+    vault_client::etcd_cert_service { 'k8s':
+      etcd_cluster => 'k8s',
+      frequency    => '1d',
+      notify       => Exec['Trigger k8s cert'],
+      require      => [ File['/etc/etcd/ssl'], User['etcd user for vault'] ],
+    }
+
+    service { 'etcd-k8s-cert.timer':
+      provider => systemd,
+      enable   => true,
+      require  => [ File['/usr/lib/systemd/system/etcd-k8s-cert.timer'], Exec['In dev mode get CA for k8s'] ],
+    }
+
+    exec { 'Trigger k8s cert':
+      command     => '/usr/bin/systemctl start etcd-k8s-cert.service',
+      user        => 'root',
+      refreshonly => true,
+    }  
   }
-  
+ 
   exec { 'In dev mode get CA for overlay':
     command => "/bin/bash -c 'source /etc/sysconfig/vault; /usr/bin/vault read -address=\$VAULT_ADDR -field=certificate \$CLUSTER_NAME/pki/etcd-overlay/cert/ca > /etc/etcd/ssl/certs/etcd-overlay.pem'",
     unless  => "/bin/bash -c 'source /etc/sysconfig/vault; /usr/bin/vault read -address=\$VAULT_ADDR -field=certificate \$CLUSTER_NAME/pki/etcd-overlay/cert/ca | diff -P /etc/etcd/ssl/certs/etcd-overlay.pem -'",
@@ -41,25 +60,6 @@ if $vault_client::role == 'master' {
 
   exec { 'update CA trust':
     command     => '/usr/bin/update-ca-trust',
-    refreshonly => true,
-  }
-
-  vault_client::etcd_cert_service { 'k8s':
-    etcd_cluster => 'k8s',
-    frequency    => '1d',
-    notify       => Exec['Trigger k8s cert'],
-    require      => [ File['/etc/etcd/ssl'], User['etcd user for vault'] ],
-  }
-
-  service { 'etcd-k8s-cert.timer':
-    provider => systemd,
-    enable   => true,
-    require  => [ File['/usr/lib/systemd/system/etcd-k8s-cert.timer'], Exec['In dev mode get CA for k8s'] ],
-  }
-
-  exec { 'Trigger k8s cert':
-    command     => '/usr/bin/systemctl start etcd-k8s-cert.service',
-    user        => 'root',
     refreshonly => true,
   }
 
