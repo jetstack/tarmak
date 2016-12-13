8b34faa9c67bd94d0bb1ab021e3e7eff1e7e0a3b
Christian Simon <simon@swine.de>
2016-12-13 14:37:41 +0000

    Fix acceptance tests

--- spec/acceptance/single_node_spec.rb
+++ spec/acceptance/single_node_spec.rb
@@ -6,26 +6,37 @@ if hosts.length == 1
       # Using puppet_apply as a helper
       it 'should work with no errors based on the example' do
         pp = <<-EOS
+$advertise_client_network = '10.0.0.0/16'
+
 etcd::instance{'k8s-main':
-  version => '3.0.15',
+  version                  => '3.0.15',
+  advertise_client_network => $advertise_client_network,
 }
 
 etcd::instance{'k8s-events':
-  version => '3.0.15',
-  client_port => 2389,
-  peer_port => 2390,
+  version                  => '3.0.15',
+  client_port              => 2389,
+  peer_port                => 2390,
+  advertise_client_network => $advertise_client_network,
 }
 
 etcd::instance{'k8s-overlay':
-  version => '2.3.7',
-  client_port => 2399,
-  peer_port => 2400,
+  version                  => '2.3.7',
+  client_port              => 2399,
+  peer_port                => 2400,
+  advertise_client_network => $advertise_client_network,
 }
         EOS
 
         # Run it twice and test for idempotency
         apply_manifest(pp, :catch_failures => true)
         expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+      end
+
+      [2379, 2389, 2399].each do |port|
+        it "test etcd on port #{port} on host #{host.name}" do
+          result = host.shell "ETCDCTL=http://127.0.0.1:#{port} /opt/etcd-3.0.15/etcdctl cluster-health"
+        end
       end
     end
   end
