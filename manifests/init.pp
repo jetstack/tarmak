d42cd978d014ea164cd2c681a4935bc4ca96e7aa
Sean Turner <sean.turner@jetstack.io>
2016-12-29 14:51:08 +0000

    Linted, validated, added rakefile

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,61 +2,61 @@ class etcd_mount
 {
   include ::systemd
 
-  file { "/usr/lib/systemd/system/attach-ebs-volume.service":
+  file { '/usr/lib/systemd/system/attach-ebs-volume.service':
     ensure => file,
-    source => "puppet:///modules/etcd_mount/attach-ebs-volume.service",
-    before => Service["attach-ebs-volume.service"],
+    source => 'puppet:///modules/etcd_mount/attach-ebs-volume.service',
+    before => Service['attach-ebs-volume.service'],
   } ~>
   Exec['systemctl-daemon-reload']
 
-  file { "/usr/lib/systemd/system/format-ebs-volume.service":
+  file { '/usr/lib/systemd/system/format-ebs-volume.service':
     ensure => file,
-    source => "puppet:///modules/etcd_mount/format-ebs-volume.service",
-    before => Service["format-ebs-volume.service"],
+    source => 'puppet:///modules/etcd_mount/format-ebs-volume.service',
+    before => Service['format-ebs-volume.service'],
   } ~>
   Exec['systemctl-daemon-reload']
 
-  file { "/usr/lib/systemd/system/var-lib-etcd.mount":
+  file { '/usr/lib/systemd/system/var-lib-etcd.mount':
     ensure => file,
-    source => "puppet:///modules/etcd_mount/var-lib-etcd.mount",
-    before => Service["var-lib-etcd.mount"],
+    source => 'puppet:///modules/etcd_mount/var-lib-etcd.mount',
+    before => Service['var-lib-etcd.mount'],
   } ~>
   Exec['systemctl-daemon-reload']
 
-  file { "/usr/local/sbin/attach_volume.sh":
+  file { '/usr/local/sbin/attach_volume.sh':
     ensure => file,
-    source => "puppet:///modules/etcd_mount/attach_volume.sh",
-    mode => '0754',
-    before => Service["attach-ebs-volume.service"],
+    source => 'puppet:///modules/etcd_mount/attach_volume.sh',
+    mode   => '0750',
+    before => Service['attach-ebs-volume.service'],
   }
 
-  file { "/usr/local/sbin/format_volume.sh":
-    ensure => file,
-    source => "puppet:///modules/etcd_mount/format_volume.sh",
-    mode => '0754',
-    before => Service["format-ebs-volume.service"],
-    require => User["etcd"],
+  file { '/usr/local/sbin/format_volume.sh':
+    ensure  => file,
+    source  => 'puppet:///modules/etcd_mount/format_volume.sh',
+    mode    => '0750',
+    before  => Service['format-ebs-volume.service'],
+    require => User['etcd'],
   }
 
-  service { "attach-ebs-volume.service":
-    enable => true,
+  service { 'attach-ebs-volume.service':
     ensure => running,
-    before => Service["format-ebs-volume.service"],
-  }
-
-  service { "format-ebs-volume.service":
     enable => true,
-    ensure => running,
-    before => Service["var-lib-etcd.mount"],
+    before => Service['format-ebs-volume.service'],
   }
 
-  service { "var-lib-etcd.mount":
+  service { 'format-ebs-volume.service':
+    ensure => running,
+    enable => true,
+    before => Service['var-lib-etcd.mount'],
+  }
+
+  service { 'var-lib-etcd.mount':
+    ensure   => running,
     provider => 'systemd',
-    ensure => running,
   }
 
-  file { "/etc/systemd/system/multiuser.target.wants/var-lib-etcd.mount":
+  file { '/etc/systemd/system/multiuser.target.wants/var-lib-etcd.mount':
     ensure => link,
-    target => "/usr/lib/systemd/system/var-lib-etcd.mount",
+    target => '/usr/lib/systemd/system/var-lib-etcd.mount',
   }
 }
