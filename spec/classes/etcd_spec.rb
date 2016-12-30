96d497d7795880c6c8434ebd62f63d0d8bd23b21
Christian Simon <simon@swine.de>
2016-12-30 14:04:04 +0000

    Provide path fact

--- spec/classes/etcd_spec.rb
+++ spec/classes/etcd_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe 'puppernetes::etcd' do
+  let(:facts) do 
+    {
+      :path => '/bin:/sbin:/usr/bin:/usr/sbin:/opt/bin'
+    }
+  end
+
   let(:pre_condition) {[
     """
 class{'vault_client': token => 'test-token'}
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,1 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-ENV['PATH']='/bin:/sbin:/usr/bin:/usr/sbin'
