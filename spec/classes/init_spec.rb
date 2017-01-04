9f3b87d474eeece7b58797502386a41e97b833e6
Sean Turner <sean.turner@jetstack.co>
2017-01-04 20:22:54 +0000

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 describe 'calico' do
   context 'with a supplied etcd cluster array' do
-    let(:params) {
-      {
-        :etcd_cluster => ['etcd1'], 
-      }
+  let(:facts) { 
+    {
+      :operatingsystem => 'CentOS',
+      :kernel => 'Linux'
+    } 
+  }
+  let(:params) {
+    {
+      :etcd_cluster => ['etcd1']
     }
+  }
     it { should contain_class('calico') }
   end
 end
