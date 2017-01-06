cbd0949b24a8cb9a25048a537bed44bead76a52c
Sean Turner <turner@worker1.jetstack.net>
2017-01-06 13:44:36 +0000

    Re-order submodules for master because aws filter doesn't converge in one run

--- manifests/master.pp
+++ manifests/master.pp
@@ -3,6 +3,6 @@ class calico::master
   class {'calico::bin_install':} ->
   class {'calico::lo_install':} ->
   class {'calico::config':} ->
-  class {'calico::node':} ->
-  class {'calico::policy_controller':}
+  class {'calico::policy_controller':} ->
+  class {'calico::node':}
 }
