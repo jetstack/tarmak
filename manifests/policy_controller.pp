3ebe8a79418da364667a1f744aba337c69f7a44c
Sean Turner <turner@worker1.jetstack.net>
2017-01-06 17:35:38 +0000

    Inheritance is bad

--- manifests/policy_controller.pp
+++ manifests/policy_controller.pp
@@ -1,30 +1,30 @@
 class calico::policy_controller (
-  $etcd_cert_path = $::calico::etcd_cert_path,
-  $policy_controller_version = $::calico::policy_controller_version,
-) inherits ::calico
+  $etcd_cert_path = $::calico::params::etcd_cert_path,
+  $policy_controller_version = $::calico::params::policy_controller_version,
+) inherits ::calico::params
 {
 
   include ::calico
 
-  file { "${::calico::helper_dir}/kubectl_helper.sh":
+  file { "${::calico::params::helper_dir}/kubectl_helper.sh":
     ensure  => file,
     content => template('calico/kubectl_helper.sh.erb'),
     mode    => '0755',
   } ->
-  file { "${::calico::secure_config_dir}/calico-config.yaml":
+  file { "${::calico::params::secure_config_dir}/calico-config.yaml":
     ensure  => file,
     content => template('calico/calico-config.yaml.erb'),
   } ->
   exec { 'deploy calico config':
-    command => "${::calico::helper_dir}/kubectl_helper.sh apply ${::calico::secure_config_dir}/calico-config.yaml",
-    unless  => "${::calico::helper_dir}/kubectl_helper.sh get ${::calico::secure_config_dir}/calico-config.yaml",
+    command => "${::calico::params::helper_dir}/kubectl_helper.sh apply ${::calico::params::secure_config_dir}/calico-config.yaml",
+    unless  => "${::calico::params::helper_dir}/kubectl_helper.sh get ${::calico::params::secure_config_dir}/calico-config.yaml",
   } ->
-  file { "${::calico::secure_config_dir}/policy-controller-deployment.yaml":
+  file { "${::calico::params::secure_config_dir}/policy-controller-deployment.yaml":
     ensure  => file,
     content => template('calico/policy-controller-deployment.yaml.erb'),
   } ->
   exec { 'deploy calico policy controller':
-    command => "${::calico::helper_dir}/kubectl_helper.sh apply ${::calico::secure_config_dir}/policy-controller-deployment.yaml",
-    unless  => "${::calico::helper_dir}/kubectl_helper.sh get ${::calico::secure_config_dir}/policy-controller-deployment.yaml",
+    command => "${::calico::params::helper_dir}/kubectl_helper.sh apply ${::calico::params::secure_config_dir}/policy-controller-deployment.yaml",
+    unless  => "${::calico::params::helper_dir}/kubectl_helper.sh get ${::calico::params::secure_config_dir}/policy-controller-deployment.yaml",
   }
 }
