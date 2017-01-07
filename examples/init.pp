cdca00a6bd2d3df51d0c954db02e31e9df202325
Sean Turner <sean.turner@jetstack.co>
2017-01-07 11:03:25 +0000

    Adds basic example

--- examples/init.pp
+++ examples/init.pp
@@ -9,4 +9,24 @@
 # Learn more about module testing here:
 # https://docs.puppet.com/guides/tests_smoke.html
 #
-include ::etcd
+
+$etcd_cluster = [ 'etcd1' ]
+
+class calico_install {
+  class { 'calico':
+    etcd_cluster => $etcd_cluster,
+  }
+}
+
+class calico_policy_controller {
+  class { 'calico::policy_controller': }
+}
+
+class calico_node_ip_pool_1 {
+  calico::ip_pool { '10.234.0.0/16':
+    ip_pool      => '10.234.0.0',
+    ip_mask      => 16,
+    ipip_enabled => 'true', #lint:ignore:quoted_booleans
+  }
+}
+
