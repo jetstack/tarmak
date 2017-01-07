76570e4f65c59ef13a0ea548dcf0104e846cc2e4
Sean Turner <sean.turner@jetstack.co>
2017-01-07 11:13:15 +0000

    Better example format

--- examples/init.pp
+++ examples/init.pp
@@ -10,23 +10,15 @@
 # https://docs.puppet.com/guides/tests_smoke.html
 #
 
-$etcd_cluster = [ 'etcd1' ]
-
-class calico_install {
-  class { 'calico':
-    etcd_cluster => $etcd_cluster,
-  }
+class { 'calico':
+  etcd_cluster => [ 'etcd1' ],
 }
 
-class calico_policy_controller {
-  class { 'calico::policy_controller': }
-}
+class { 'calico::policy_controller': }
 
-class calico_node_ip_pool_1 {
-  calico::ip_pool { '10.234.0.0/16':
-    ip_pool      => '10.234.0.0',
-    ip_mask      => 16,
-    ipip_enabled => 'true', #lint:ignore:quoted_booleans
-  }
+calico::ip_pool { '10.234.0.0/16':
+  ip_pool      => '10.234.0.0',
+  ip_mask      => 16,
+  ipip_enabled => 'true', #lint:ignore:quoted_booleans
 }
 
