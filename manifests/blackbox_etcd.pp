00f5facf81fd18ec4131cd4480f92ca94e1e350c
Sean Turner <sean.turner@jetstack.io>
2017-01-25 14:37:29 +0000

    Updates the other dependency

--- manifests/blackbox_etcd.pp
+++ manifests/blackbox_etcd.pp
@@ -43,6 +43,6 @@ class prometheus::blackbox_etcd (
   service { 'blackbox_exporter':
     ensure  => running,
     enable  => true,
-    require => [ File[ "${dest_dir}/blackbox_exporter"], File["${dest_dir}/blackbox_exporter.yaml"], File["${systemd_path}/blackbox_exporter.service.erb"]],
+    require => [ File[ "${dest_dir}/blackbox_exporter"], File["${dest_dir}/blackbox_exporter.yaml"], File["${systemd_path}/blackbox_exporter.service"]],
   }
 }
