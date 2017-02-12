fdd3585a28249bfdafcd2770abce69da774c123c
Christian Simon <simon@swine.de>
2017-02-12 13:03:04 +0000

    Fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-ENV['PATH']='/bin:/sbin:/usr/bin:/usr/sbin'
+
+RSpec.configure do |config|
+  config.default_facts = {
+    :osfamily => 'RedHat',
+    :path => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
+end
