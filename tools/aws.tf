246eb200bd498daf2cce02c632e2d6b878d8087f
Christian Simon <simon@swine.de>
2017-03-07 10:33:53 +0000

    Adds ELBs for jenkins/foreman
    * Ditch ALBs not ready for showtime yet

--- tools/aws.tf
+++ tools/aws.tf
@@ -2,3 +2,10 @@ provider "aws" {
   region              = "${var.region}"
   allowed_account_ids = ["${var.allowed_account_ids}"]
 }
+
+data "aws_acm_certificate" "wildcard" {
+  domain   = "*.${data.terraform_remote_state.network.public_zones[0]}"
+  statuses = ["ISSUED"]
+}
+
+data "aws_elb_hosted_zone_id" "main" {}
