7ba28ed97172cb26f7adab85542583fbfe001b85
Christian Simon <simon@swine.de>
2017-03-10 15:11:57 +0000

    Fix bastion SSH access

--- kubernetes/bastion_security_group_rules.tf
+++ kubernetes/bastion_security_group_rules.tf
@@ -3,8 +3,8 @@ resource "aws_security_group_rule" "kubernetes_master_allow_bastion_ssh" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.kubernetes_master.id}"
-  security_group_id        = "${data.terraform_remote_state.hub_tools.bastion_security_group_id}"
+  source_security_group_id = "${data.terraform_remote_state.hub_tools.bastion_security_group_id}"
+  security_group_id        = "${aws_security_group.kubernetes_master.id}"
 }
 
 resource "aws_security_group_rule" "kubernetes_worker_allow_bastion_ssh" {
@@ -12,8 +12,8 @@ resource "aws_security_group_rule" "kubernetes_worker_allow_bastion_ssh" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.kubernetes_worker.id}"
-  security_group_id        = "${data.terraform_remote_state.hub_tools.bastion_security_group_id}"
+  source_security_group_id = "${data.terraform_remote_state.hub_tools.bastion_security_group_id}"
+  security_group_id        = "${aws_security_group.kubernetes_worker.id}"
 }
 
 resource "aws_security_group_rule" "etcd_allow_bastion_ssh" {
@@ -21,6 +21,6 @@ resource "aws_security_group_rule" "etcd_allow_bastion_ssh" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.etcd.id}"
-  security_group_id        = "${data.terraform_remote_state.hub_tools.bastion_security_group_id}"
+  source_security_group_id = "${data.terraform_remote_state.hub_tools.bastion_security_group_id}"
+  security_group_id        = "${aws_security_group.etcd.id}"
 }
