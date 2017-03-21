2ace7f4a082f61ae762ad81a3d9034e8105d7043
Christian Simon <simon@swine.de>
2017-03-21 16:06:34 +0000

    Adds elasticsearch / kibana / fluentd

---
+++ manifests/kibana.pp
@@ -0,0 +1,19 @@
+class kubernetes_addons::kibana(
+  String $namespace=$::kubernetes_addons::params::namespace,
+  String $image='gcr.io/google_containers/kibana',
+  String $version='v4.6.1-1',
+  String $request_cpu='50m',
+  String $request_mem='128Mi',
+  String $limit_cpu='100m',
+  String $limit_mem='256Mi',
+  Integer $replicas=2,
+) inherits ::kubernetes_addons::params {
+  require ::kubernetes
+
+  kubernetes::apply{'kibana':
+    manifests => [
+      template('kubernetes_addons/kibana-svc.yaml.erb'),
+      template('kubernetes_addons/kibana-deployment.yaml.erb'),
+    ],
+  }
+}
