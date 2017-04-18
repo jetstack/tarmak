a1118a53916101fa1089a872f28a3ca8d5d1d671
Christian Simon <simon@swine.de>
2017-04-18 11:26:26 +0100

    Update image versions for calico

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,8 +1,8 @@
 class calico::node (
   String $node_image = 'quay.io/calico/node',
-  String $node_version = '1.1.0',
+  String $node_version = '1.1.1',
   String $cni_image = 'quay.io/calico/cni',
-  String $cni_version = '1.6.1',
+  String $cni_version = '1.6.2',
   String $ipv4_pool_cidr = '10.231.0.0/16',
   Enum['always', 'cross-subnet', 'off'] $ipv4_pool_ipip_mode = 'always',
 )
