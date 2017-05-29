b295a2cb6f6f68c3313ef4f931486001f9389603
Christian Simon <simon@swine.de>
2017-05-29 14:45:18 +0100

    Upgrade Kube DNS to make it compatible with kubernetes 1.6 and RBAC

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -1,15 +1,13 @@
 # == Class kubernetes::dns
 class kubernetes::dns(
-  $image='gcr.io/google_containers/kubedns-amd64',
-  $version='1.9',
-  $dnsmasq_image='gcr.io/google_containers/kube-dnsmasq-amd64',
-  $dnsmasq_version='1.4',
-  $dnsmasq_metrics_image='gcr.io/google_containers/dnsmasq-metrics-amd64',
-  $dnsmasq_metrics_version='1.0',
-  $exechealthz_image='gcr.io/google_containers/exechealthz-amd64',
-  $exechealthz_version='1.2',
+  $image='gcr.io/google_containers/k8s-dns-kube-dns-amd64',
+  $version='1.14.2',
+  $dnsmasq_image='gcr.io/google_containers/k8s-dns-sidecar-amd64',
+  $dnsmasq_version='1.14.2',
+  $sidecar_image='gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64',
+  $sidecar_version='1.14.2',
   $autoscaler_image='gcr.io/google_containers/cluster-proportional-autoscaler-amd64',
-  $autoscaler_version='1.0.0',
+  $autoscaler_version='1.1.1-r2',
   $min_replicas=3,
 ){
   require ::kubernetes
@@ -17,11 +15,27 @@ class kubernetes::dns(
   $cluster_domain = $::kubernetes::cluster_domain
   $cluster_dns = $::kubernetes::_cluster_dns
 
+  $authorization_mode = $::kubernetes::_authorization_mode
+  if member($authorization_mode, 'RBAC'){
+    $rbac_enabled = true
+  } else {
+    $rbac_enabled = false
+  }
+
+  if versioncmp($::kubernetes::version, '1.6.0') >= 0 {
+    $version_before_1_6 = false
+  } else {
+    $version_before_1_6 = true
+  }
+
   kubernetes::apply{'kube-dns':
     manifests => [
+      template('kubernetes/kube-dns-config-map.yaml.erb'),
+      template('kubernetes/kube-dns-service-account.yaml.erb'),
       template('kubernetes/kube-dns-deployment.yaml.erb'),
       template('kubernetes/kube-dns-svc.yaml.erb'),
       template('kubernetes/kube-dns-horizontal-autoscaler-deployment.yaml.erb'),
+      template('kubernetes/kube-dns-horizontal-autoscaler-rbac.yaml.erb'),
     ],
   }
 }
