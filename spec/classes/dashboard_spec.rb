5a69acf99dc7e5f641aee4b176c7203865ec4659
Christian Simon <simon@swine.de>
2017-06-14 14:33:48 +0100

    Adds minikube tests for dashboard

--- spec/classes/dashboard_spec.rb
+++ spec/classes/dashboard_spec.rb
@@ -28,5 +28,29 @@ describe 'kubernetes_addons::dashboard' do
       expect(manifests[1]).to match(%{^[-\s].*cpu: [0-9]+})
       expect(manifests[1]).to match(%{^[-\s].*memory: [0-9]+})
     end
+
+    context 'minikube tests' do
+      after(:each) do
+          kubectl_delete(manifests) if ENV['MINIKUBE'] == 'true'
+      end
+
+      it 'deploy healthy app', :minikube => true do
+        skip "Minikube tests disabled by default, pass MINIKUBE=true as env var" if ENV['MINIKUBE'] != 'true'
+        expect(
+          minikube_apply(manifests)
+        ).to eq(0)
+
+        retries = 10
+        begin
+          ready_replicas = kubectl_get('deployment','kube-system', 'kubernetes-dashboard')['status']['readyReplicas']
+          raise "deployment not healthy" if ready_replicas != 1
+        rescue Exception => e
+          retries -= 1
+          raise e if retries == 0
+          sleep 5
+          retry
+        end
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,103 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'open3'
+require 'logger'
+
+$logger = Logger.new(STDERR)
+$logger.level = Logger::DEBUG
+
+module MinikubeHelpers
+  def help
+    :available
+  end
+
+  def cmd(*cmd)
+    Open3.popen3(*cmd) do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+      $logger.debug "cmd=#{cmd} stdout=#{stdout.read} stderr=#{stderr.read} exitcode=#{wait_thr.value.exitstatus}"
+      wait_thr.value.exitstatus
+    end
+  end
+
+  def cmd_stdout(*cmd)
+    Open3.popen3(*cmd) do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+      $logger.debug "cmd=#{cmd} stderr=#{stderr.read} exitcode=#{wait_thr.value.exitstatus}"
+      stdout.read
+    end
+  end
+
+  def minikube_cmd(subcmd)
+    return ['minikube', subcmd, '--profile', @minikube_profile]
+  end
+
+  def minikube_start
+    cmd(
+      *minikube_cmd('start'),
+      '--extra-config=apiserver.Authorization.Mode=RBAC',
+      "--kubernetes-version=v#{@minikube_kubernetes_version}",
+    )
+  end
+
+  def minikube_status
+    cmd(*minikube_cmd('status'))
+  end
+
+  def kubectl_apply(manifests)
+    kubectl_stdin('apply', manifests)
+  end
+
+  def kubectl_delete(manifests)
+    kubectl_stdin('delete', manifests)
+  end
+
+  def kubectl_create(manifests)
+    kubectl_stdin('create', manifests)
+  end
+
+  def kubectl_stdin(subcmd, manifests)
+    cmd = ['kubectl', subcmd, '-f', '-']
+    Open3.popen3(*cmd) do |stdin, stdout, stderr, wait_thr|
+      manifests.each do |manifest|
+        stdin.write("---\n")
+        stdin.write(manifest)
+      end
+      stdin.close
+      $logger.debug "cmd=#{cmd} stdout=#{stdout.read} stderr=#{stderr.read} exitcode=#{wait_thr.value.exitstatus}"
+      return wait_thr.value.exitstatus
+    end
+  end
+
+  def kubectl_get(object, namespace, name)
+    return JSON.parse cmd_stdout(
+      'kubectl', 'get', object, '--namespace', namespace, '-o', 'json', name
+    )
+  end
+
+  def minikube_wait_deployment_ready(namespace, name, replicas)
+    status = JSON.parse kubectl_get('deployment', namespace, name)
+    return status
+  end
+
+  def minikube_prepare
+    @minikube_profile = "kubernetes-addons"
+    @minikube_kubernetes_version = ENV['KUBERNETES_VERSION'] || '1.6.4'
+    ENV['KUBECONFIG'] = "#{Dir.pwd}/kubeconfig"
+    if minikube_status != 0
+      minikube_start
+    end
+  end
+
+  def minikube_apply(manifests)
+    minikube_prepare
+    kubectl_apply(manifests)
+  end
+end
 
 RSpec.configure do |config|
   config.default_facts = {
     :path => '/bin:/sbin:/usr/bin:/usr/sbin:/opt/bin',
     :osfamily => 'RedHat',
   }
+  config.include MinikubeHelpers
+
 end
