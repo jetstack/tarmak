c0f84c9aa94af1d7c10e1c355f45f356ccda7f57
Christian Simon <simon@swine.de>
2017-06-16 13:45:30 +0100

    Adds RBAC to nginx-ingress

--- manifests/nginx_ingress.pp
+++ manifests/nginx_ingress.pp
@@ -10,11 +10,28 @@ class kubernetes_addons::nginx_ingress(
   $host_port=false,
 ) inherits ::kubernetes_addons::params {
   require ::kubernetes
+  require ::kubernetes_addons::default_backend
+
+  $deployment_name = 'nginx-ingress-lb'
+
+  $authorization_mode = $::kubernetes::_authorization_mode
+  if member($authorization_mode, 'RBAC'){
+    $rbac_enabled = true
+  } else {
+    $rbac_enabled = false
+  }
+
+  if versioncmp($::kubernetes::version, '1.6.0') >= 0 {
+    $version_before_1_6 = false
+  } else {
+    $version_before_1_6 = true
+  }
 
   kubernetes::apply{'nginx-ingress':
     manifests => [
       template('kubernetes_addons/nginx-ingress-svc.yaml.erb'),
       template('kubernetes_addons/nginx-ingress-deployment.yaml.erb'),
+      template('kubernetes_addons/nginx-ingress-rbac.yaml.erb'),
     ],
   }
 }
--- spec/classes/nginx_ingress_spec.rb
+++ spec/classes/nginx_ingress_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 describe 'kubernetes_addons::nginx_ingress' do
   let(:pre_condition) do
     "
-      class kubernetes{}
+      class kubernetes{
+        $_authorization_mode = ['RBAC']
+        $version = '1.6.4'
+      }
       define kubernetes::apply(
         $manifests,
       ){}
@@ -11,6 +14,10 @@ describe 'kubernetes_addons::nginx_ingress' do
 
   let(:manifests) do
     catalogue.resource('Kubernetes::Apply', 'nginx-ingress').send(:parameters)[:manifests]
+  end
+
+  let(:default_backend_manifests) do
+    catalogue.resource('Kubernetes::Apply', 'default-backend').send(:parameters)[:manifests]
   end
 
   context 'with defaults' do
@@ -27,6 +34,36 @@ describe 'kubernetes_addons::nginx_ingress' do
     it 'have resources set' do
       expect(manifests[1]).to match(%{^[-\s].*cpu: [0-9]+})
       expect(manifests[1]).to match(%{^[-\s].*memory: [0-9]+})
+    end
+
+    context 'minikube tests' do
+      after(:each) do
+          kubectl_delete(default_backend_manifests) if @minikube_cleanup
+          kubectl_delete(manifests) if @minikube_cleanup
+      end
+
+      it 'deploy healthy app', :minikube => true do
+        @minikube_cleanup = false
+        expect(
+          minikube_apply(default_backend_manifests)
+        ).to eq(0)
+
+        @minikube_cleanup = true
+        expect(
+          minikube_apply(manifests)
+        ).to eq(0)
+
+        retries = 10
+        begin
+          ready_replicas = kubectl_get('deployment','kube-system', 'nginx-ingress-lb')['status']['readyReplicas']
+          raise "deployment not healthy" if ready_replicas != 1
+        rescue Exception => e
+          retries -= 1
+          raise e if retries == 0
+          sleep 5
+          retry
+        end
+      end
     end
   end
 end
