5655c98ce43623450a591417e71e19a597e447ba
Christian Simon <simon@swine.de>
2017-06-20 09:40:13 +0100

    Renamed to tarmak

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,12 +2,12 @@
 class calico::params {
   $etcd_cert_path = '/etc/etcd/ssl'
   $etcd_cert_base_name = 'etcd-overlay'
-  if defined('::puppernetes') {
-    $etcd_cluster = $::puppernetes::_etcd_cluster
-    $etcd_overlay_port = $::puppernetes::etcd_overlay_client_port
-    $bin_dir = $::puppernetes::bin_dir
-    $systemd_dir = $::puppernetes::systemd_dir
-    $cloud_provider = $::puppernetes::cloud_provider
+  if defined('::tarmak') {
+    $etcd_cluster = $::tarmak::_etcd_cluster
+    $etcd_overlay_port = $::tarmak::etcd_overlay_client_port
+    $bin_dir = $::tarmak::bin_dir
+    $systemd_dir = $::tarmak::systemd_dir
+    $cloud_provider = $::tarmak::cloud_provider
   } else {
     $etcd_cluster = []
     $etcd_overlay_port = 2359
