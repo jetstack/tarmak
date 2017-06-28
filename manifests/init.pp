02e3272634e9d15c7e9d64af2c8117face54073c
Christian Simon <simon@swine.de>
2017-06-28 10:47:38 +0100

    Move dir module to modules

---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+class prometheus(
+  $role = '',
+  $etcd_cluster = $::prometheus::params::etcd_cluster,
+  $etcd_k8s_port = $::prometheus::params::etcd_k8s_port,
+  $etcd_events_port = $::prometheus::params::etcd_events_port,
+  $etcd_overlay_port = $::prometheus::params::etcd_overlay_port,
+  $blackbox_download_url = $::prometheus::params::blackbox_download_url,
+  $blackbox_dest_dir = $::prometheus::params::blackbox_dest_dir,
+  $blackbox_config_dir = $::prometheus::params::blackbox_config_dir,
+  $systemd_path = $::prometheus::params::systemd_path,
+  $node_exporter_image = $::prometheus::params::node_exporter_image,
+  $node_exporter_version = $::prometheus::params::node_exporter_version,
+  $node_exporter_port = $::prometheus::params::node_exporter_port,
+  $addon_dir = $::prometheus::params::addon_dir,
+  $helper_dir = $::prometheus::params::helper_dir,
+  $prometheus_namespace = $::prometheus::params::prometheus_namespace,
+  $prometheus_image = $::prometheus::params::prometheus_image,
+  $prometheus_version = $::prometheus::params::prometheus_version,
+  $prometheus_storage_local_retention = $::prometheus::params::prometheus_storage_local_retention,
+  $prometheus_storage_local_memchunks = $::prometheus::params::prometheus_storage_local_memchunks,
+  $prometheus_port = $::prometheus::params::prometheus_port,
+  $prometheus_use_module_config = $::prometheus::params::prometheus_use_module_config,
+  $prometheus_use_module_rules = $::prometheus::params::prometheus_use_module_rules,
+  $prometheus_install_state_metrics = $::prometheus::params::prometheus_install_state_metrics,
+  $prometheus_install_node_exporter = $::prometheus::params::prometheus_install_node_exporter
+) inherits ::prometheus::params
+{
+  if $role == 'etcd' {
+    include prometheus::blackbox_etcd
+    include prometheus::node_exporter_service
+  }
+
+  if $role == 'master' {
+    include prometheus::prometheus_deployment
+  }
+}
---
+++ manifests/params.pp
@@ -0,0 +1,33 @@
+class prometheus::params {
+  $blackbox_download_url = 'https://github.com/jetstack-experimental/blackbox_exporter/releases/download/poc-proxy/'
+  $blackbox_dest_dir = '/usr/local/sbin'
+  $blackbox_config_dir = '/etc/blackbox'
+  $systemd_path = '/etc/systemd/system'
+  $addon_dir = '/etc/kubernetes/addons'
+  $helper_dir = '/usr/local/sbin'
+  $node_exporter_image = 'prom/node-exporter'
+  $node_exporter_version = 'v0.13.0'
+  $node_exporter_port = 9100
+  $prometheus_namespace = 'monitoring'
+  $prometheus_image = 'quay.io/prometheus/prometheus'
+  $prometheus_version = 'v1.4.1'
+  $prometheus_port = 9090
+  $prometheus_storage_local_retention = '6h'
+  $prometheus_storage_local_memchunks = 500000
+  $prometheus_use_module_config = true
+  $prometheus_use_module_rules = true
+  $prometheus_install_state_metrics = true
+  $prometheus_install_node_exporter = true
+
+  if defined('::tarmak') {
+    $etcd_cluster = $::tarmak::_etcd_cluster
+    $etcd_k8s_port = $::tarmak::etcd_k8s_main_client_port
+    $etcd_events_port = $::tarmak::etcd_k8s_events_client_port
+    $etcd_overlay_port = $::tarmak::etcd_overlay_client_port
+  } else {
+    $etcd_cluster = undef
+    $etcd_k8s_port = 2379
+    $etcd_events_port = 2369
+    $etcd_overlay_port = 2359
+  }
+}
