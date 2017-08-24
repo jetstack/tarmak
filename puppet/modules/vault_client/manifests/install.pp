22591b0f0e23d8c1da6546d13e3e10f0ac2c81f9
Joshua  Van Leeuwen <joshua.vanleeuwen@jetstack.io>
2017-08-24 10:49:37 +0100

    vault-helper is installed through puppet

--- puppet/modules/vault_client/manifests/install.pp
+++ puppet/modules/vault_client/manifests/install.pp
@@ -1,8 +1,8 @@
 class vault_client::install {
-    $vault_helper_path = "${::vault_client::dest_dir}/vault-helper-${vault_client::version}"
-    $vault_helper_bin = '/opt/bin/vault-helper'
 
-    file { $::vault_client::dest_dir:
+    $vault_helper_path = "${::vault_client::dest_dir}/vault-helper-${vault_client::version}"
+
+    file { $::vault_client::dest_dir: #/opt
         ensure => directory,
         mode   => '0755',
     }
@@ -12,20 +12,24 @@ class vault_client::install {
     }
     -> exec {"vault-helper-${vault_client::version}-download":
         command => "curl -sL ${::vault_client::download_url} -o ${vault_helper_path}/vault-helper",
-        creates => $vault_helper_path,
         path    => ['/usr/bin', '/bin'],
     }
-    -> file { "${vault_helper_path}/vault-helper":
+    -> file {"${vault_helper_path}/vault-helper":
         ensure => 'file',
         mode   => '0755',
         owner  => 'root',
         group  => 'root',
     }
-    -> file { $vault_helper_bin:
+    -> file { "${::vault_client::bin_dir}/vault-helper":
         ensure => 'link',
         mode   => '0755',
         owner  => 'root',
         group  => 'root',
-        target => "${vault_helper_path}/vault-helper",
+        target => "${::vault_client::dest_dir}/vault-helper-${vault_client::version}/vault-helper",
     }
+
+    ensure_resource('file', [$::vault_client::bin_dir], {
+        ensure => directory,
+        mode   => '0755',
+    })
 }
