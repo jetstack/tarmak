4bcdba08c54d587c729757b2216de43728ed5b05
Christian Simon <simon@swine.de>
2017-08-25 17:20:40 +0100

    Reenable kubernetes acceptance tests

--- puppet/modules/kubernetes/spec/spec_helper_acceptance.rb
+++ puppet/modules/kubernetes/spec/spec_helper_acceptance.rb
@@ -3,7 +3,9 @@ require 'beaker-rspec'
 $module_path = '/etc/puppetlabs/code/modules/'
 
 # Install Puppet on all hosts
-install_puppet_on(hosts, options)
+install_puppet_agent_on(hosts, {
+  :puppet_agent_version          => '1.9.1',
+})
 
 RSpec.configure do |c|
   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
@@ -17,8 +19,7 @@ RSpec.configure do |c|
         on host, 'yum install -y rsync'
       end
       logger.notify "ensure rsync exists on #{host}"
-      install_dev_puppet_module_on(host, :source => module_root, :module_name => 'kubernetes', :target_module_path => $module_path)
-      rsync_to(host, "#{module_root}/spec/fixtures/modules/", $module_path, {})
+      rsync_to(host, "#{module_root}/../", $module_path, {})
       on host, "chown -R 0:0 #{$module_path}"
     end
   end
