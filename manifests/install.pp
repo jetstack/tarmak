7d0d4a8789171a7e287365e5a88a5ab3c90fed63
Joshua  Van Leeuwen <joshua.vanleeuwen@jetstack.io>
2017-08-29 15:07:51 +0100

    Corrected curl exec and token renew service

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,6 +12,7 @@ class vault_client::install {
     }
     -> exec {"vault-helper-${vault_client::version}-download":
         command => "curl -sL ${::vault_client::download_url} -o ${vault_helper_path}/vault-helper",
+        creates => "${vault_helper_path}/vault-helper",
         path    => ['/usr/bin', '/bin'],
     }
     -> file {"${vault_helper_path}/vault-helper":
