09159e3181341492b194b1f73b783046932c15f1
Luke Addison <luke@Lukes-MacBook-Pro.local>
2017-09-06 13:11:47 +0100

    Correct default location for tarmak config in help text

--- cmd/tarmak/cmd/root.go
+++ cmd/tarmak/cmd/root.go
@@ -32,7 +32,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tarmak.yaml)")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tarmak/tarmak.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
