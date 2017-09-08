15ea2ffd3cb075ebff331df52dc9159fa37ab553
Joshua  Van Leeuwen <joshua.vanleeuwen@jetstack.io>
2017-09-08 14:20:54 +0100

    Updated vault-helper to 0.8.7

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,13 +5,12 @@
 #
 class vault_client::params {
   $app_name = 'vault-helper'
-  $version = '0.8.6'
+  $version = '0.8.7'
   $bin_dir = '/opt/bin'
   $dest_dir = '/opt'
   $config_dir = '/etc/vault'
   $download_dir = '/tmp'
-  #$download_url = 'https://github.com/jetstack-experimental/vault-helper/releases/download/#VERSION#/vault-helper_#VERSION#_linux_amd64'
-  $download_url = 'https://github.com/JoshVanL/temp/raw/master/vault-helper'
+  $download_url = 'https://github.com/jetstack-experimental/vault-helper/releases/download/#VERSION#/vault-helper_#VERSION#_linux_amd64'
   $server_url = 'http://127.0.0.1:8200'
   $systemd_dir = '/etc/systemd/system'
 }
