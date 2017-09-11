081580c462d69ac0de28ee4808b0255149d7abd4
Christian Simon <simon@swine.de>
2017-09-11 23:32:52 +0100

    Adds stacknames as constants

---
+++ pkg/apis/tarmak/v1alpha1/constants.go
@@ -0,0 +1,9 @@
+package v1alpha1
+
+const (
+	StackNameState      = "state"
+	StackNameNetwork    = "network"
+	StackNameTools      = "tools"
+	StackNameVault      = "vault"
+	StackNameKubernetes = "kubernetes"
+)
