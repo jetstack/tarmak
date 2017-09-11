6bfdabeafd7b2131ca2702545be146178e5e5eed
Christian Simon <simon@swine.de>
2017-09-11 23:33:07 +0100

    Refactor packer to support multiple base images

---
+++ pkg/packer/image.go
@@ -0,0 +1,119 @@
+package packer
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	logrus "github.com/Sirupsen/logrus"
+
+	tarmakDocker "github.com/jetstack/tarmak/pkg/docker"
+	"github.com/jetstack/tarmak/pkg/tarmak/interfaces"
+)
+
+type image struct {
+	packer *Packer
+	log    *logrus.Entry
+	tarmak interfaces.Tarmak
+
+	environment string
+	imageName   string
+	id          *string
+}
+
+func (i *image) tags() map[string]string {
+	return map[string]string{
+		PackerTagEnvironment:   i.environment,
+		PackerTagBaseImageName: i.imageName,
+	}
+}
+
+func (i *image) ImageID() (id string, err error) {
+	if i.id != nil {
+		return *i.id, nil
+	}
+
+	id, err = i.tarmak.Context().Environment().Provider().QueryImage(
+		i.tags(),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	i.id = &id
+
+	return id, nil
+
+}
+
+func (i *image) Build() (amiID string, err error) {
+	c := i.packer.Container()
+
+	rootPath, err := i.tarmak.RootPath()
+	if err != nil {
+		return "", fmt.Errorf("error getting rootPath: %s", err)
+	}
+
+	// set tarmak environment vars vars
+	for key, value := range i.tags() {
+		c.Env = append(c.Env, fmt.Sprintf("%s=%s", strings.ToUpper(key), value))
+	}
+
+	// get aws secrets
+	if environmentProvider, err := i.tarmak.Context().Environment().Provider().Environment(); err != nil {
+		return "", fmt.Errorf("error getting environment secrets from provider: %s", err)
+	} else {
+		c.Env = append(c.Env, environmentProvider...)
+	}
+
+	c.WorkingDir = "/packer"
+	c.Cmd = []string{"sleep", "3600"}
+
+	err = c.Prepare()
+	if err != nil {
+		return "", err
+	}
+
+	// make sure container get's cleaned up
+	defer c.CleanUpSilent(i.log)
+
+	buildSourcePath := filepath.Join(
+		rootPath,
+		"packer",
+		fmt.Sprintf("%s.json", i.imageName),
+	)
+
+	buildContent, err := ioutil.ReadFile(buildSourcePath)
+	if err != nil {
+		return "", err
+	}
+
+	buildPath := "build.json"
+
+	buildTar, err := tarmakDocker.TarStreamFromFile(buildPath, string(buildContent))
+	if err != nil {
+		return "", err
+	}
+
+	err = c.UploadToContainer(buildTar, "/packer")
+	if err != nil {
+		return "", err
+	}
+	i.log.Debug("copied packer build state")
+
+	err = c.Start()
+	if err != nil {
+		return "", fmt.Errorf("error starting container: %s", err)
+	}
+
+	returnCode, err := c.Execute("packer", []string{"build", buildPath})
+	if err != nil {
+		return "", err
+	}
+	if exp, act := 0, returnCode; exp != act {
+		return "", fmt.Errorf("unexpected return code: exp=%d, act=%d", exp, act)
+	}
+
+	return "unknown", nil
+}
--- pkg/packer/packer.go
+++ pkg/packer/packer.go
@@ -1,11 +1,6 @@
 package packer
 
 import (
-	"fmt"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
-
 	logrus "github.com/Sirupsen/logrus"
 
 	tarmakDocker "github.com/jetstack/tarmak/pkg/docker"
@@ -19,8 +14,6 @@ type Packer struct {
 	*tarmakDocker.App
 	log    *logrus.Entry
 	tarmak interfaces.Tarmak
-
-	imageID *string
 }
 
 func New(tarmak interfaces.Tarmak) *Packer {
@@ -38,105 +31,24 @@ func New(tarmak interfaces.Tarmak) *Packer {
 		tarmak: tarmak,
 		log:    log,
 	}
-	for key, val := range p.tags() {
-		p.log = p.log.WithField(key, val)
-	}
 
 	return p
 }
 
-func (p *Packer) tags() map[string]string {
-	return map[string]string{
-		PackerTagEnvironment:   p.tarmak.Context().Environment().Name(),
-		PackerTagBaseImageName: p.tarmak.Context().BaseImage(),
-	}
-}
+func (p *Packer) images() (images []*image) {
+	environment := p.tarmak.Context().Environment().Name()
+	for _, imageName := range p.tarmak.Context().Images() {
+		image := &image{
+			environment: environment,
+			imageName:   imageName,
+		}
+		image.log = p.log
+		for key, val := range image.tags() {
+			image.log = image.log.WithField(key, val)
+		}
 
-func (p *Packer) QueryAMIID() (amiID string, err error) {
-	if p.imageID != nil {
-		return *p.imageID, nil
-	}
-
-	imageID, err := p.tarmak.Context().Environment().Provider().QueryImage(
-		p.tags(),
-	)
-	if err != nil {
-		return "", err
+		images = append(images, image)
 	}
 
-	p.imageID = &imageID
-
-	return imageID, nil
-
-}
-
-func (p *Packer) Build() (amiID string, err error) {
-	c := p.Container()
-
-	rootPath, err := p.tarmak.RootPath()
-	if err != nil {
-		return "", fmt.Errorf("error getting rootPath: %s", err)
-	}
-
-	// set tarmak environment vars vars
-	for key, value := range p.tags() {
-		c.Env = append(c.Env, fmt.Sprintf("%s=%s", strings.ToUpper(key), value))
-	}
-
-	// get aws secrets
-	if environmentProvider, err := p.tarmak.Context().Environment().Provider().Environment(); err != nil {
-		return "", fmt.Errorf("error getting environment secrets from provider: %s", err)
-	} else {
-		c.Env = append(c.Env, environmentProvider...)
-	}
-
-	c.WorkingDir = "/packer"
-	c.Cmd = []string{"sleep", "3600"}
-
-	err = c.Prepare()
-	if err != nil {
-		return "", err
-	}
-
-	// make sure container get's cleaned up
-	defer c.CleanUpSilent(p.log)
-
-	buildSourcePath := filepath.Join(
-		rootPath,
-		"packer",
-		fmt.Sprintf("%s.json", p.tarmak.Context().BaseImage()),
-	)
-
-	buildContent, err := ioutil.ReadFile(buildSourcePath)
-	if err != nil {
-		return "", err
-	}
-
-	buildPath := "build.json"
-
-	buildTar, err := tarmakDocker.TarStreamFromFile(buildPath, string(buildContent))
-	if err != nil {
-		return "", err
-	}
-
-	err = c.UploadToContainer(buildTar, "/packer")
-	if err != nil {
-		return "", err
-	}
-	p.log.Debug("copied packer build state")
-
-	err = c.Start()
-	if err != nil {
-		return "", fmt.Errorf("error starting container: %s", err)
-	}
-
-	returnCode, err := c.Execute("packer", []string{"build", buildPath})
-	if err != nil {
-		return "", err
-	}
-	if exp, act := 0, returnCode; exp != act {
-		return "", fmt.Errorf("unexpected return code: exp=%d, act=%d", exp, act)
-	}
-
-	return "unknown", nil
+	return images
 }
