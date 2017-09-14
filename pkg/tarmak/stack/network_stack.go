52a8d620d915dd880586f378ec9cbaca8f1d1c0b
Christian Simon <simon@swine.de>
2017-09-14 13:47:21 +0100

    Assign stack name to network stack

--- pkg/tarmak/stack/network_stack.go
+++ pkg/tarmak/stack/network_stack.go
@@ -1,8 +1,10 @@
 package stack
 
 import (
+	"fmt"
 	"net"
 
+	tarmakv1alpha1 "github.com/jetstack/tarmak/pkg/apis/tarmak/v1alpha1"
 	"github.com/jetstack/tarmak/pkg/tarmak/interfaces"
 )
 
@@ -15,31 +17,30 @@ type NetworkStack struct {
 var _ interfaces.Stack = &NetworkStack{}
 
 func newNetworkStack(s *Stack) (*NetworkStack, error) {
-	// TODO: refactor me
-	/*
-		s.name = config.StackNameNetwork
-		_, net, err := net.ParseCIDR(conf.NetworkCIDR)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing network: %s", err)
-		}
-	*/
+	s.name = tarmakv1alpha1.StackNameNetwork
+
+	// TODO: refactor me, read network from somewhere
+	_, net, err := net.ParseCIDR("172.18.0.0/16")
+	if err != nil {
+		return nil, fmt.Errorf("error parsing network: %s", err)
+	}
 
 	return &NetworkStack{
-		Stack: s,
-		//networkCIDR: net,
+		Stack:       s,
+		networkCIDR: net,
 	}, nil
 
 }
 
 func (s *NetworkStack) Variables() map[string]interface{} {
 	output := map[string]interface{}{}
+	if s.networkCIDR != nil {
+		output["network"] = s.networkCIDR
+	}
 	// TODO: refactor me
 	/*
 		n := s.Stack.conf.Network
 
-		if s.networkCIDR != nil {
-			output["network"] = s.networkCIDR
-		}
 		if n.PeerContext != "" {
 			output["vpc_peer_stack"] = n.PeerContext
 		}
