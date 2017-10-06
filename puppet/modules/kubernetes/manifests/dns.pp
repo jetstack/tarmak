8c8c5cc0c5946c04fc9a00e99e85bd6395a63454
Christian Simon <simon@swine.de>
2017-10-06 15:39:38 +0100

    Upgrade kubedns to fix CVE-2017-14491

--- puppet/modules/kubernetes/manifests/dns.pp
+++ puppet/modules/kubernetes/manifests/dns.pp
@@ -1,11 +1,11 @@
 # == Class kubernetes::dns
 class kubernetes::dns(
   $image='gcr.io/google_containers/k8s-dns-kube-dns-amd64',
-  $version='1.14.2',
+  $version='1.14.5',
   $dnsmasq_image='gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64',
-  $dnsmasq_version='1.14.2',
+  $dnsmasq_version='1.14.5',
   $sidecar_image='gcr.io/google_containers/k8s-dns-sidecar-amd64',
-  $sidecar_version='1.14.2',
+  $sidecar_version='1.14.5',
   $autoscaler_image='gcr.io/google_containers/cluster-proportional-autoscaler-amd64',
   $autoscaler_version='1.1.1-r2',
   $min_replicas=3,
