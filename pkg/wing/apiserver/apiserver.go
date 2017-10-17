40390ed7db67156ea79b4d3a9a0fe30b738d377a
Christian Simon <simon@swine.de>
2017-10-17 07:24:00 +0100

    Upgrade apiserver code to support release-1.8
    * use recommendConfig
    * following https://github.com/kubernetes/sample-apiserver/tree/release-1.8

--- pkg/wing/apiserver/apiserver.go
+++ pkg/wing/apiserver/apiserver.go
@@ -46,7 +46,7 @@ func init() {
 }
 
 type Config struct {
-	GenericConfig *genericapiserver.Config
+	GenericConfig *genericapiserver.RecommendedConfig
 }
 
 // WingServer contains state for a Kubernetes cluster master/api server.
@@ -55,29 +55,31 @@ type WingServer struct {
 }
 
 type completedConfig struct {
-	*Config
+	GenericConfig genericapiserver.CompletedConfig
+}
+
+type CompletedConfig struct {
+	// Embed a private pointer that cannot be instantiated outside of this package.
+	*completedConfig
 }
 
 // Complete fills in any fields not set that are required to have valid data. It's mutating the receiver.
-func (c *Config) Complete() completedConfig {
-	c.GenericConfig.Complete()
+func (cfg *Config) Complete() CompletedConfig {
+	c := completedConfig{
+		cfg.GenericConfig.Complete(),
+	}
 
 	c.GenericConfig.Version = &version.Info{
 		Major: "1",
 		Minor: "0",
 	}
 
-	return completedConfig{c}
-}
-
-// SkipComplete provides a way to construct a server instance without config completion.
-func (c *Config) SkipComplete() completedConfig {
-	return completedConfig{c}
+	return CompletedConfig{&c}
 }
 
 // New returns a new instance of WingServer from the given config.
 func (c completedConfig) New() (*WingServer, error) {
-	genericServer, err := c.Config.GenericConfig.SkipComplete().New("wing", genericapiserver.EmptyDelegate) // completion is done in Complete, no need for a second time
+	genericServer, err := c.GenericConfig.New("wing", genericapiserver.EmptyDelegate)
 	if err != nil {
 		return nil, err
 	}
