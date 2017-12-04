4c0478b0475430820663ed75d93ecd7d24ffff31
Christian Simon <simon@swine.de>
2017-12-04 18:27:26 +0000

    Ensure flags are parsed
    * prevent error message from controller loop

--- cmd/wing/cmd/root.go
+++ cmd/wing/cmd/root.go
@@ -2,6 +2,7 @@
 package cmd
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -16,6 +17,8 @@ var RootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	flag.CommandLine.Parse([]string{})
+
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
