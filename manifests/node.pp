cd4044bc11b3c5adf8bb386c556ca884023e8ff0
Christian Simon <simon@swine.de>
2018-01-29 13:57:54 +0000

    Enable metrics endpoint for felix daemon

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,3 +1,8 @@
+# Calico Node
+#
+# Calico Node contains a Daemon Set that spinsup the overlay network on every
+# workern node.
+# @param metrics_port Port for felix metrics endpoint, 0 disables metrics collection
 class calico::node (
   String $node_image = 'quay.io/calico/node',
   String $node_version = '2.5.1',
@@ -5,6 +10,7 @@ class calico::node (
   String $cni_version = '1.10.0',
   String $ipv4_pool_cidr = '10.231.0.0/16',
   Enum['always', 'cross-subnet', 'off'] $ipv4_pool_ipip_mode = 'always',
+  Integer[0,65535] $metrics_port = 9091,
 )
 {
   include ::kubernetes
