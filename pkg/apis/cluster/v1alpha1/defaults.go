d5c8ba4cb909df4c46f54347bbea087ef41d0275
Christian Simon <simon@swine.de>
2018-02-20 16:06:36 +0000

    Update default version of kubernetes to 1.8.8

--- pkg/apis/cluster/v1alpha1/defaults.go
+++ pkg/apis/cluster/v1alpha1/defaults.go
@@ -38,7 +38,7 @@ func SetDefaults_Cluster(obj *Cluster) {
 
 	// set default kubernetes version
 	if obj.Kubernetes.Version == "" {
-		obj.Kubernetes.Version = "1.7.10"
+		obj.Kubernetes.Version = "1.8.8"
 	}
 
 	// zone
