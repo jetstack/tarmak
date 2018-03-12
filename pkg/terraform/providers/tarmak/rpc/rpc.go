b8eaa7bb8782dcb8345e2e22b21c3d98c2863781
Christian Simon <simon@swine.de>
2018-03-12 09:57:51 +0000

    Add tarmak provider for terraform
    The terraform provider is just a shim implementation that calls out to
    tarmak using a RPC connection. The only supported resource type is a
    `data.tarmak_bastion_instance` which will block until the bastion is
    fully setup.

---
+++ pkg/terraform/providers/tarmak/rpc/rpc.go
@@ -0,0 +1,68 @@
+// Copyright Jetstack Ltd. See LICENSE for details.
+package rpc
+
+import (
+	"io"
+	"net/rpc"
+
+	"github.com/alecthomas/multiplex"
+	"github.com/sirupsen/logrus"
+
+	"github.com/jetstack/tarmak/pkg/tarmak/interfaces"
+)
+
+const (
+	ConnectorSocket = "/tmp/tarmak-connector.sock"
+	RPCName         = "Tarmak"
+)
+
+type tarmakInterface struct {
+}
+
+type tarmakRPC struct {
+	tarmak interfaces.Tarmak
+	stack  interfaces.Stack
+}
+
+func (r *tarmakRPC) log() *logrus.Entry {
+	return r.tarmak.Log()
+}
+
+func NewTarmak(tarmak interfaces.Tarmak, stack interfaces.Stack) Tarmak {
+	return &tarmakRPC{tarmak: tarmak, stack: stack}
+}
+
+type Tarmak interface {
+	BastionInstanceStatus(*BastionInstanceStatusArgs, *BastionInstanceStatusReply) error
+	Ping(*PingArgs, *PingReply) error
+}
+
+// bind a new rpc server to socket
+func Bind(log *logrus.Entry, tarmak Tarmak, reader io.Reader, writer io.Writer, closer io.Closer) {
+
+	s := rpc.NewServer()
+	s.RegisterName(RPCName, tarmak)
+
+	log.Debugf("rpc server started")
+
+	mx := multiplex.MultiplexedServer(struct {
+		io.Reader
+		io.Writer
+		io.Closer
+	}{reader, writer, closer},
+	)
+	for {
+		c, err := mx.Accept()
+		if err != nil {
+			log.Warnf("error accepting rpc connection: %s", err)
+			break
+		}
+		go func(c *multiplex.Channel) {
+			log.Debugf("new rpc connection")
+			s.ServeConn(c)
+			log.Debugf("closed rpc connection")
+		}(c)
+	}
+
+	log.Debugf("rpc server stopped")
+}
