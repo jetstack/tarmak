8d4eddec62c9b3cad1d218e0ca17e1fc710378f1
Luke Addison <luke.addison@jetstack.io>
2018-03-16 16:10:11 +0000

    Include PSPs for master node and add variable declaration

--- puppet/modules/kubernetes/manifests/pod_security_policy.pp
+++ puppet/modules/kubernetes/manifests/pod_security_policy.pp
@@ -1,7 +1,9 @@
 # This class manages RBAC manifests
 class kubernetes::pod_security_policy{
   require ::kubernetes
-  if $_pod_security_policy {
+  
+  $pod_security_policy = $::kubernetes::_pod_security_policy
+  if $pod_security_policy {
     kubernetes::apply{'pod-security-policy':
       manifests => [
         template('kubernetes/pod-security-policy.yaml.erb'),
