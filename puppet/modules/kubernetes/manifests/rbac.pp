7e3b63c9318b9decd8599db216cb7b027c99ac5d
Luke Addison <luke.addison@jetstack.io>
2018-03-17 17:36:40 +0000

    Apply PodSecurityPolicy RBAC seperately

--- puppet/modules/kubernetes/manifests/rbac.pp
+++ puppet/modules/kubernetes/manifests/rbac.pp
@@ -2,7 +2,7 @@
 class kubernetes::rbac{
   require ::kubernetes
 
-  $authorization_mode = $kubernetes::_authorization_mode
+  $authorization_mode = $kubernetes::_authorization_mode  
   if member($authorization_mode, 'RBAC') and versioncmp($::kubernetes::version, '1.6.0') >= 0 {
     kubernetes::apply{'puppernetes-rbac':
       manifests => [
@@ -12,6 +12,15 @@ class kubernetes::rbac{
         template('kubernetes/rbac-namespace-roles.yaml.erb'),
         template('kubernetes/rbac-namespace-role-bindings.yaml.erb'),
       ],
+    }
+
+    $pod_security_policy = $::kubernetes::_pod_security_policy
+    if $pod_security_policy {
+      kubernetes::apply{'puppernetes-rbac-psp':
+        manifests => [
+          template('kubernetes/pod-security-policy-rbac.yaml.erb'),
+        ],
+      }
     }
   }
 }
