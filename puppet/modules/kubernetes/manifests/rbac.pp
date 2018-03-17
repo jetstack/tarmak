3f4ac486ec22ff6ca95c7ae2ea289cdde98c0ac8
Luke Addison <luke.addison@jetstack.io>
2018-03-17 16:10:05 +0000

    Fix RBAC version check

--- puppet/modules/kubernetes/manifests/rbac.pp
+++ puppet/modules/kubernetes/manifests/rbac.pp
@@ -3,7 +3,7 @@ class kubernetes::rbac{
   require ::kubernetes
 
   $authorization_mode = $kubernetes::_authorization_mode
-  if member($authorization_mode, 'RBAC') and versioncmp($::kubernetes::version, '1.6.0') < 0 {
+  if member($authorization_mode, 'RBAC') and versioncmp($::kubernetes::version, '1.6.0') >= 0 {
     kubernetes::apply{'puppernetes-rbac':
       manifests => [
         template('kubernetes/rbac-namespace-kube-public.yaml.erb'),
