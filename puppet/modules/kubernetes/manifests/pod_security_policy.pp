b0a629ab4670aaaf1deeeba82cf764f0ad5f171f
Luke Addison <luke.addison@jetstack.io>
2018-03-19 10:51:08 +0000

    Move PSP RBAC into PSP manifest

--- puppet/modules/kubernetes/manifests/pod_security_policy.pp
+++ puppet/modules/kubernetes/manifests/pod_security_policy.pp
@@ -4,8 +4,14 @@ class kubernetes::pod_security_policy{
   
   $pod_security_policy = $::kubernetes::_pod_security_policy
   if $pod_security_policy {
-    kubernetes::apply{'pod-security-policy':
+
+    if ! member($authorization_mode, 'RBAC') {
+      fail('RBAC should be enabled when PodSecurityPolicy is enabled.')
+    }
+
+    kubernetes::apply{'puppernetes-rbac-psp':
       manifests => [
+        template('kubernetes/pod-security-policy-rbac.yaml.erb'),
         template('kubernetes/pod-security-policy.yaml.erb'),
       ],
     }
