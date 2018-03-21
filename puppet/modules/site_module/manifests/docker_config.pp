cf8d07d4ec7b68512714c14e0ffe7b86b01c51ec
Christian Simon <simon@swine.de>
2018-03-21 08:28:31 +0000

    Put container runtime resources into a separate slice
    This ensures we can reserve CPU and memory resources for the system and
    the runtime properly in a follow-up PR

--- puppet/modules/site_module/manifests/docker_config.pp
+++ puppet/modules/site_module/manifests/docker_config.pp
@@ -3,4 +3,10 @@ class site_module::docker_config {
     ensure  => file,
     content => template('site_module/docker.erb'),
   }
+  file { '/etc/systemd/system/docker.service.d':
+    ensure  => directory,
+  } -> file { '/etc/systemd/system/docker.service.d/10-slice.conf':
+    ensure  => directory,
+    content => '[Service]\nSlice=podruntime.slice\n',
+  }
 }
