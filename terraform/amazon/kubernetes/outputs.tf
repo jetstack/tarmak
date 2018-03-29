d536ada863980d3682dc730c5000b550dced066f
Luke Addison <luke.addison@jetstack.io>
2018-03-29 10:52:54 +0100

    remove output

--- terraform/amazon/kubernetes/outputs.tf
+++ terraform/amazon/kubernetes/outputs.tf
@@ -2,6 +2,6 @@ output "bastion_instance_id" {
   value = "${module.bastion.bastion_instance_id}"
 }
 
-output "instance_fqdns" {
+/*output "instance_fqdns" {
   value = ["${module.vault.instance_fqdns}"]
-}
+}*/
