e76744137fe934724a7e276bb08f2c392bc90bb0
Luke Addison <luke.addison@jetstack.io>
2018-04-11 18:24:06 +0100

    Fix arrays

--- terraform/amazon/modules/network/nat.tf
+++ terraform/amazon/modules/network/nat.tf
@@ -6,6 +6,6 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "main" {
   depends_on    = ["aws_internet_gateway.main"]
   count         = "${length(var.availability_zones)}"
-  allocation_id = "${aws_eip.nat.*.id[count.index]}"
-  subnet_id     = "${aws_subnet.public.*.id[count.index]}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
 }
