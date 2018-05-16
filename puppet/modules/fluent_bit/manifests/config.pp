2abd1977372d0f4bb0261914749a437e7a38c3e2
Luke Addison <luke.addison@jetstack.io>
2018-05-16 15:00:02 +0100

    Remove default null output

--- puppet/modules/fluent_bit/manifests/config.pp
+++ puppet/modules/fluent_bit/manifests/config.pp
@@ -15,13 +15,4 @@ class fluent_bit::config {
     content => template('fluent_bit/td-agent-bit.conf.erb'),
   }
 
-  $types = ['all']
-  file { '/etc/td-agent-bit/td-agent-bit-output.conf':
-    ensure  => file,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => template('fluent_bit/td-agent-bit-output.conf.erb'),
-  }
-
 }
