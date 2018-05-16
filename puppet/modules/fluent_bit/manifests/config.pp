31c6a8272944615a489d313232da8061cf07b7c1
Christian Simon <simon@swine.de>
2018-05-16 09:11:38 +0100

    Add audit log forwarding only on masters with enabled audit log

--- puppet/modules/fluent_bit/manifests/config.pp
+++ puppet/modules/fluent_bit/manifests/config.pp
@@ -1,5 +1,12 @@
 class fluent_bit::config {
 
+  if defined(Class['kubernetes::apiserver']) and $::kubernetes::apiserver::audit_enabled {
+    $audit_enabled = true
+    $audit_log_path = $::kubernetes::apiserver::audit_log_path
+  } else {
+    $audit_enabled = false
+  }
+
   file { '/etc/td-agent-bit/td-agent-bit.conf':
     ensure  => file,
     mode    => '0644',
