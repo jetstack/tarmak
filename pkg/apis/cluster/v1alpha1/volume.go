4c8f8b2a7d23f180f11222c39a28149dc14ed324
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-07-23 15:16:39 +0100

    Revert "Remove The Kubicorn Authors copyright note"
    This reverts commit 9d0d908ca3de6ed9854cb2c89328dc5e0df93b71.

--- pkg/apis/cluster/v1alpha1/volume.go
+++ pkg/apis/cluster/v1alpha1/volume.go
@@ -1,4 +1,5 @@
 // Copyright Jetstack Ltd. See LICENSE for details.
+// Copyright © 2017 The Kubicorn Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
