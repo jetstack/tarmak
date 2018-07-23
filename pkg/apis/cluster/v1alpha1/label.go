1fab9f9768a1b2bf89b894644006c4870d0a16fd
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-07-23 11:28:57 +0100

    Remove API machinery fields

--- pkg/apis/cluster/v1alpha1/label.go
+++ pkg/apis/cluster/v1alpha1/label.go
@@ -14,15 +14,8 @@
 
 package v1alpha1
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
 // Label structure for instancepool node labels
 type Label struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
 	Key   string `json:"key,omitempty"`
 	Value string `json:"value,omitempty"`
 }
