f27c7630ef77155af4a3cff1455b5a1141ad3050
JoshVanL <vleeuwenjoshua@gmail.com>
2018-07-27 17:57:09 +0100

    Adds consul template files

---
+++ puppet/modules/consul/manifests/consul-backup.service.pp
@@ -0,0 +1,38 @@
+include ::tarmak
+define consul::consul_backup(
+    String $region,
+    String $backup_bucket_prefix,
+    String $consul_master_token,
+)
+{
+    require consul
+
+    $service_name = "consul-backup"
+
+    file { "${::consul::systemd_dir}/${service_name}.service":
+        ensure  => file,
+        content => template('consul/consul-backup.service.erb'),
+        notify  => Exec["${service_name}-systemctl-daemon-reload"],
+        mode    => '0644'
+    }
+    ~> exec { "${service_name}-systemctl-daemon-reload":
+        command     => 'systemctl daemon-reload',
+        refreshonly => true,
+        path        => $::consul::path,
+    }
+    -> service { "${service_name}.service":
+        ensure => 'running',
+        enable => true,
+    }
+
+    file { "${vault_client::systemd_dir}/${service_name}.timer":
+        ensure  => file,
+        content => template('vault_client/cert.timer.erb'),
+        notify  => Exec["${service_name}-systemctl-daemon-reload"],
+    }
+    ~> service { "${service_name}.timer":
+        ensure  => 'running',
+        enable  => true,
+        require => Exec["${service_name}-systemctl-daemon-reload"],
+    }
+}
---
+++ puppet/modules/consul/manifests/consul.pp
@@ -0,0 +1,60 @@
+include ::tarmak
+define consul::consul(
+    String $fqdn,
+    String $private_ip,
+    String $consul_master_token,
+    String $region,
+    String $sonul_encrypt,
+    String $instance_count,
+    Integer instance_count,
+    String $environment,
+)
+{
+    require consul
+
+    $mount_name = 'var-lib-consul'
+    $service_name = 'consul'
+    $json_name = 'consul'
+    $hcl_name = 'vault'
+
+    file { "${::consul::systemd_dir}/${mount_name}.mount":
+        ensure  => file,
+        content => template('consul/var-lib-consul.mount.erb'),
+        notify  => Exec["${service_name}-systemctl-daemon-reload"],
+        mode    => '0644'
+    }
+
+    file { "etc/consul/${json_name}.json":
+        ensure  => file,
+        content => template('consul/consul.json.erb'),
+        mode    => '0600'
+    }
+
+    file { "etc/vault/${hcl_name}.hcl":
+        ensure  => file,
+        content => template('consul/vault.hcl.erb'),
+        mode    => '0600'
+    }
+
+    file { "/usr/local/bin/download-vault-consul.sh":
+        ensure  => file,
+        content => template('consul/download-vault-consul.sh.erb'),
+        mode    => '-755'
+    }
+
+    file { "${::vault_client::systemd_dir}/${service_name}.service":
+        ensure  => file,
+        content => template('vault_client/consul.service.erb'),
+        notify  => Service["${service_name}.service"],
+        mode    => '0644'
+    }
+    ~> exec { "${service_name}-systemctl-daemon-reload":
+        command     => 'systemctl daemon-reload',
+        refreshonly => true,
+        path        => $::vault_client::path,
+    }
+    -> service { "${service_name}.service":
+        ensure => 'running',
+        enable => true,
+    }
+}
