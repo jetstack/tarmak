3f85772fbeec84426fcabbeda4c481f3b059f760
JoshVanL <vleeuwenjoshua@gmail.com>
2018-07-27 17:57:09 +0100

    Adds consul template files

---
+++ puppet/modules/consul/manifests/attach-ebs-volume.service.pp
@@ -0,0 +1,27 @@
+include ::tarmak
+define consul::attach_ebs_volume(
+    String $region,
+    String $volume,
+    String $volume_id,
+)
+{
+    require consul
+
+    $service_name = "attach-ebs-volume"
+
+    file { "${::consul::systemd_dir}/${service_name}.service":
+        ensure  => file,
+        content => template('consul/attach-ebs-volume.service.erb'),
+        notify  => Exec["${service_name}-systemctl-daemon-reload"],
+        mode    => '0644'
+    }
+    ~> exec { "${service_name}-systemctl-daemon-reload":
+        command     => 'systemctl daemon-reload',
+        refreshonly => true,
+        path        => $::consul::path,
+    }
+    -> service { "${service_name}.service":
+        ensure => 'running',
+        enable => true,
+    }
+}
---
+++ puppet/modules/consul/manifests/ensure-ebs-volume-formatted.service.pp
@@ -0,0 +1,24 @@
+include ::tarmak
+define consul::ensure_ebs_volume(
+)
+{
+    require consul
+
+    $service_name = "ensure-ebs-volume-formatted"
+
+    file { "${::consul::systemd_dir}/${service_name}.service":
+        ensure  => file,
+        content => template('consul/ensure-ebs-volume-formatted.service.erb'),
+        notify  => Exec["${service_name}-systemctl-daemon-reload"],
+        mode    => '0644'
+    }
+    ~> exec { "${service_name}-systemctl-daemon-reload":
+        command     => 'systemctl daemon-reload',
+        refreshonly => true,
+        path        => $::consul::path,
+    }
+    -> service { "${service_name}.service":
+        ensure => 'running',
+        enable => true,
+    }
+}
