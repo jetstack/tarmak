8d247d66dd64230d4534d726f5a2f31bfca25286
Christian Simon <simon@swine.de>
2018-08-07 11:24:09 +0100

    Update k8s versions used in tests

--- pkg/apis/cluster/v1alpha1/defaults.go
+++ pkg/apis/cluster/v1alpha1/defaults.go
@@ -38,7 +38,7 @@ func SetDefaults_Cluster(obj *Cluster) {
 
 	// set default kubernetes version
 	if obj.Kubernetes.Version == "" {
-		obj.Kubernetes.Version = "1.9.7"
+		obj.Kubernetes.Version = "1.9.10"
 	}
 
 	// zone
