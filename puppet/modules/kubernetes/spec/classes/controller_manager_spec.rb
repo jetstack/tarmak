ae34793dac25a5119dfbe8a673e26491901c40cd
JoshVanL <vleeuwenjoshua@gmail.com>
2018-08-09 17:55:34 +0100

    Adds tests for controller_manager_spec and scheduler_spec
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- puppet/modules/kubernetes/spec/classes/controller_manager_spec.rb
+++ puppet/modules/kubernetes/spec/classes/controller_manager_spec.rb
@@ -61,4 +61,40 @@ describe 'kubernetes::controller_manager' do
       it { should_not contain_file(service_file).with_content(%r{--use-service-account-credentials}) }
     end
   end
+
+  context 'feature gates' do
+    context 'without given feature gates and not enabled pod priority' do
+      let(:pre_condition) {[
+        """
+        class{'kubernetes': controller_feature_gates => []}
+        """
+      ]}
+      it 'should have default feature gates' do
+        should_not contain_file(service_file).with_content(/#{Regexp.escape('--feature-gates=')}/)
+      end
+    end
+
+    context 'without given feature gates and enabled pod priority' do
+      let(:pre_condition) {[
+        """
+        class{'kubernetes': enable_pod_priority => true}
+        """
+      ]}
+      let(:version) { '1.6.0' }
+      it 'should have default feature gates' do
+        should contain_file(service_file).with_content(/#{Regexp.escape('--feature-gates=PodPriority=true')}/)
+      end
+    end
+
+    context 'with given feature gates' do
+      let(:pre_condition) {[
+        """
+        class{'kubernetes': controller_feature_gates => ['foo=true', 'bar=true']}
+        """
+      ]}
+      it 'should have custom feature gates' do
+        should contain_file(service_file).with_content(/#{Regexp.escape('--feature-gates=foo=true,bar=true')}/)
+      end
+    end
+  end
 end
