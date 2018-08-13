244b102cbe204d3c8b9ca23b351b4ef627243781
JoshVanL <vleeuwenjoshua@gmail.com>
2018-08-13 14:59:18 +0100

    Adds support for kube-proxy for >= 1.11
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- puppet/modules/kubernetes/manifests/proxy.pp
+++ puppet/modules/kubernetes/manifests/proxy.pp
@@ -7,6 +7,7 @@ class kubernetes::proxy(
   Array[String] $systemd_requires = [],
   Array[String] $systemd_after = [],
   Array[String] $systemd_before = [],
+  String $proxy_config_file = "${::kubernetes::config_dir}/kube-proxy-config.yaml",
 ){
   require ::kubernetes
 
@@ -16,6 +17,8 @@ class kubernetes::proxy(
   $_systemd_after = $systemd_after
   $_systemd_requires = $systemd_after
   $_systemd_before = $systemd_before
+
+  $post_1_11 = versioncmp($::kubernetes::version, '1.11.0') >= 0
 
   # ensure ipvsadm and contrack installed (for kube-proxy)
   $conntrack_package_name = 'conntrack'
@@ -31,6 +34,16 @@ class kubernetes::proxy(
     group   => $kubernetes::group,
     content => template('kubernetes/kubeconfig.erb'),
     notify  => Service["${service_name}.service"],
+  }
+
+  if $post_1_11 {
+    file{$proxy_config_file:
+      ensure  => file,
+      mode    => '0640',
+      owner   => 'root',
+      group   => $kubernetes::group,
+      content => template('kubernetes/kube-proxy-config.yaml.erb'),
+    }
   }
 
   kubernetes::symlink{'proxy':}
