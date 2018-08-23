6e46e8c2d708b21375cf2eeb5e98355266088ef9
JoshVanL <vleeuwenjoshua@gmail.com>
2018-08-23 17:37:44 +0100

    Improve sorting of devices

--- puppet/modules/aws_ebs/spec/functions/disks_spec.rb
+++ puppet/modules/aws_ebs/spec/functions/disks_spec.rb
@@ -22,7 +22,39 @@ describe 'aws_ebs::disks' do
         }
       end
 
-      $exp_result = ['abc', 'cba', 'zzz']
+      it { is_expected.to run.with_params().and_return(['abc', 'cba', 'zzz']) }
+    end
+
+    context 'sort disks by correct logical order' do
+      let :facts do {
+          :disks => {
+            'nvme3n5' => {
+              'size' => '50.00 GiB',
+            },
+            'nvme0n1' => {
+              'size' => '50.00 GiB',
+            },
+            'nvme10n1' => {
+              'size' => '50.00 GiB',
+            },
+            'nvme2n2' => {
+              'size' => '50.00 GiB',
+            },
+            'nvme11n2' => {
+              'size' => '50.00 GiB',
+            },
+            'nvme2n1' => {
+              'size' => '50.00 GiB',
+            },
+            'nvme11n1' => {
+              'size' => '50.00 GiB',
+            },
+          }
+        }
+      end
+
+      $exp_result = ['nvme0n1', 'nvme2n1', 'nvme2n2', 'nvme3n5', 'nvme10n1', 'nvme11n1', 'nvme11n2']
       it { is_expected.to run.with_params().and_return($exp_result) }
+
     end
 end
