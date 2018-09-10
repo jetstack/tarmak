3280ccf679b2e4d39dbbbda8e728cd89762d253d
JoshVanL <vleeuwenjoshua@gmail.com>
2018-09-10 16:13:26 +0100

    KMS encrypt puppet tar ball in S3
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- terraform/amazon/modules/kubernetes/iam_policies.tf
+++ terraform/amazon/modules/kubernetes/iam_policies.tf
@@ -4,6 +4,7 @@ data "template_file" "iam_tarmak_bucket_read" {
   vars {
     puppet_tar_gz_bucket_path = "${var.secrets_bucket}/${aws_s3_bucket_object.puppet-tar-gz.key}"
     wing_binary_path          = "${var.secrets_bucket}/${data.template_file.stack_name.rendered}/wing-*"
+    vault_kms_key_id          = "${var.vault_kms_key_id}"
   }
 }
 
--- terraform/amazon/modules/kubernetes/puppet_s3.tf
+++ terraform/amazon/modules/kubernetes/puppet_s3.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket_object" "puppet-tar-gz" {
+  #key          = "${data.template_file.stack_name.rendered}/${md5(file("puppet.tar.gz"))}-puppet.tar.gz"
   key          = "${data.template_file.stack_name.rendered}/puppet.tar.gz"
   bucket       = "${var.secrets_bucket}"
   content_type = "application/tar+gzip"
   source       = "puppet.tar.gz"
-  etag         = "${md5(file("puppet.tar.gz"))}"
+  kms_key_id   = "${var.vault_kms_key_id}"
 }
