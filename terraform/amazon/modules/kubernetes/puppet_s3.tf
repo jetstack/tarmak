1b44b0b5dd506bccac38d9c3f01607b733e10b17
JoshVanL <vleeuwenjoshua@gmail.com>
2018-09-11 17:46:47 +0100

    Use encrypted puppet discovered from hash
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- terraform/amazon/modules/kubernetes/puppet_s3.tf
+++ terraform/amazon/modules/kubernetes/puppet_s3.tf
@@ -1,8 +1,15 @@
 resource "aws_s3_bucket_object" "puppet-tar-gz" {
-  #key          = "${data.template_file.stack_name.rendered}/${md5(file("puppet.tar.gz"))}-puppet.tar.gz"
-  key          = "${data.template_file.stack_name.rendered}/puppet.tar.gz"
+  key          = "${data.template_file.stack_name.rendered}/${md5(file("puppet.tar.gz"))}-puppet.tar.gz"
   bucket       = "${var.secrets_bucket}"
   content_type = "application/tar+gzip"
   source       = "puppet.tar.gz"
   kms_key_id   = "${var.vault_kms_key_id}"
+}
+
+resource "aws_s3_bucket_object" "latest-puppet-hash" {
+  key          = "${data.template_file.stack_name.rendered}/latest-puppet-hash"
+  bucket       = "${var.secrets_bucket}"
+  content_type = "application/tar+gzip"
+  content      = "${md5(file("puppet.tar.gz"))}"
+  etag         = "${md5(file("puppet.tar.gz"))}"
 }
