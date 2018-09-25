9274d48ff0694df6a6b885f3b9e7d348a869f89d
JoshVanL <vleeuwenjoshua@gmail.com>
2018-09-25 10:34:10 +0100

    Dereference resource only available in wing-dev-mode
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- pkg/tarmak/interfaces/interfaces.go
+++ pkg/tarmak/interfaces/interfaces.go
@@ -130,7 +130,6 @@ type Provider interface {
 	AskEnvironmentLocation(Initialize) (string, error)
 	AskInstancePoolZones(Initialize) (zones []string, err error)
 	UploadConfiguration(Cluster, io.ReadSeeker, string) error
-	VerifyInstanceTypes(intstancePools []InstancePool) error
 	EnsureRemoteResources() error
 }
 
