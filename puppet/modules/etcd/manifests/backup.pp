ff605a78e1235fb37cc3b1324f75e63cacb85f9d
JoshVanL <vleeuwenjoshua@gmail.com>
2018-09-25 15:26:10 +0100

    Move backup etcd to define on each cluster
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- puppet/modules/etcd/manifests/backup.pp
+++ puppet/modules/etcd/manifests/backup.pp
@@ -1,13 +1,17 @@
 define etcd::backup (
   String $backup_bucket_prefix = $etcd::backup_bucket_prefix,
-  String $backup_schedule = $etcd::backup_schedule,
-  String $endpoints = '',
+  String $version = $etcd::version,
+  Integer $client_port = 0,
   String $ca_path = '',
 ){
   include ::etcd
 
   $service_name = "etcd-${name}"
   $backup_service_name = "${service_name}-backup"
+  $endpoints = "127.0.0.1:${client_port}"
+
+  $hour = fqdn_rand(24, $name)
+  $backup_schedule = "*-*-* ${hour}:00:00"
 
   exec { "${service_name}-systemctl-daemon-reload":
     command     => 'systemctl daemon-reload',
