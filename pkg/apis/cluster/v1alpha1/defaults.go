3bbcc3ffd4b8f0b42f997fc0da8273f25881d0e7
Christian Simon <simon@swine.de>
2018-11-22 11:18:19 +0000

    Update kubernetes versions
    * New default is 1.11
    * Add 1.12, 1.13
    
    Signed-off-by: Christian Simon <simon@swine.de>

--- pkg/apis/cluster/v1alpha1/defaults.go
+++ pkg/apis/cluster/v1alpha1/defaults.go
@@ -38,7 +38,7 @@ func SetDefaults_Cluster(obj *Cluster) {
 
 	// set default kubernetes version
 	if obj.Kubernetes.Version == "" {
-		obj.Kubernetes.Version = "1.10.6"
+		obj.Kubernetes.Version = "1.11.4"
 	}
 
 	// zone
