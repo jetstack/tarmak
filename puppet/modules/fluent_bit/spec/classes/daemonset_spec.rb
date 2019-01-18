587f992b13d6d0341cd5b1a264b39a395c765da6
JoshVanL <vleeuwenjoshua@gmail.com>
2019-01-18 17:56:51 +0000

    Fix puppet tests using old kebectl apply
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- puppet/modules/fluent_bit/spec/classes/daemonset_spec.rb
+++ puppet/modules/fluent_bit/spec/classes/daemonset_spec.rb
@@ -6,19 +6,11 @@ describe 'fluent_bit::daemonset' do
     ]
   end
 
-  let :service_file do
-    '/etc/systemd/system/kubectl-apply-fluent-bit.service'
-  end
-
   let :manifests_file do
     '/etc/kubernetes/apply/fluent-bit.yaml'
   end
 
   context 'with default values for all parameters' do
-    it 'should write systemd unit for applying' do
-      should contain_file(service_file).with_content(/User=kubernetes/)
-    end
-
     it 'should write manifests' do
       should contain_file(manifests_file).with_content(/#{Regexp.escape('*_kube-system_*,*_service-broker_*,*_monitoring_*')}/)
       should contain_file(manifests_file).with_content(/#{Regexp.escape('10.254.0.1 kubernetes.default.svc.cluster.local')}/)
