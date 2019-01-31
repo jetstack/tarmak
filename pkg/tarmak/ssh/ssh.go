048fb892d5e09dd057214e92c1254ae04edf065d
JoshVanL <vleeuwenjoshua@gmail.com>
2019-01-31 12:22:19 +0000

    Stop warning about failed bastion conn and forget
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- pkg/tarmak/ssh/ssh.go
+++ pkg/tarmak/ssh/ssh.go
@@ -332,7 +332,9 @@ func (s *SSH) bastionClient() (*ssh.Client, error) {
 				return s.bastionClientConn, nil
 			}
 		}
+
 		s.log.Warnf("current connection to bastion failed: %s", err)
+		s.bastionClientConn = nil
 	}
 
 	conf, err := s.config()
