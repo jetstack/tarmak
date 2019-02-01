c28080bf69d5efb81dd679254f671a978d143eca
Christian Simon <simon@swine.de>
2019-02-01 10:26:34 +0000

    Fix function to verify yaml
    Signed-off-by: Christian Simon <simon@swine.de>

--- puppet/modules/kubernetes/functions/addon_manager_labels.pp
+++ puppet/modules/kubernetes/functions/addon_manager_labels.pp
@@ -1,12 +1,18 @@
 function kubernetes::addon_manager_labels(String $manifests) {
   $reconcile_label = 'addonmanager.kubernetes.io/mode: Reconcile'
   $ensure_exists_label = 'addonmanager.kubernetes.io/mode: EnsureExists'
+
   $yaml_array = split($manifests, '---')
 
   $yaml_array.each |String $yaml| {
-    if $yaml.strip != '' and split($yaml, '\n').length > 1 {
-      if !($yaml =~ $reconcile_label or $yaml =~ $ensure_exists_label) {
+    if $yaml.strip != '' {
+      $yaml_obj = parseyaml($yaml)
+      $addon_manager_label_value = $yaml_obj.dig('metadata','labels','addonmanager.kubernetes.io/mode')
+      if $addon_manager_label_value == undef {
         fail("yaml did not contain addon manager label: ${yaml}")
+      }
+      if $addon_manager_label_value != 'Reconcile' and $addon_manager_label_value != 'EnsureExists' {
+        fail("yaml did contain incorrect addon manager label value: ${addon_manager_label_value}")
       }
     }
   }
