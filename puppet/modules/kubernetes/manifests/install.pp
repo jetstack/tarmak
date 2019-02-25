b11f1db9d737ffa3533bfc55aa17a0252897fe4a
JoshVanL <vleeuwenjoshua@gmail.com>
2019-02-25 10:26:37 +0000

    Increases inotify limits on Kubernetes instances
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- puppet/modules/kubernetes/manifests/install.pp
+++ puppet/modules/kubernetes/manifests/install.pp
@@ -1,5 +1,8 @@
 # download and install hyperkube
-class kubernetes::install {
+class kubernetes::install (
+  Integer $max_user_instances = $::kubernetes::max_user_instances,
+  Integer $max_user_watches = $::kubernetes::max_user_watches,
+){
   include kubernetes
 
   $hyperkube_path = "${::kubernetes::_dest_dir}/hyperkube"
@@ -18,5 +21,19 @@ class kubernetes::install {
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
+  }
+
+  exec {'sysctl-system':
+    command     => 'sysctl --system',
+    refreshonly => true,
+    path        => ['/usr/bin/', '/bin', '/usr/sbin'],
+  }
+
+  file{"${::kubernetes::params::sysctl_dir}/fs.conf":
+    ensure  => file,
+    mode    => '0640',
+    owner   => 'root',
+    content => template('kubernetes/fs.conf.erb'),
+    notify  => Exec['sysctl-system'],
   }
 }
